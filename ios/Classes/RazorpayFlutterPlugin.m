57f2235e8c7d261b5def8a5f7c077204a6675b55
irajeshh <85046336+irajeshh@users.noreply.github.com>
2025-11-26 11:09:34 +0530

    Android, ios, test files removed
    Deleted all Android and iOS source files, configuration, and test files related to the Razorpay Flutter plugin, as well as screenshots. This removes the plugin implementation and associated resources from the repository.

--- ios/Classes/RazorpayFlutterPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "RazorpayFlutterPlugin.h"
-#import <razorpay_web/razorpay_web-Swift.h>
-
-@implementation RazorpayFlutterPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftRazorpayFlutterPlugin registerWithRegistrar:registrar];
-}
-@end
